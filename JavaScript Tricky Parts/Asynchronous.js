0ac63e363c854ce142fc10c32d48cad88be980e0
Arman Abkar <armanw@outlook.com>
2021-04-13 00:08:08 +0430

    update folders structure

--- JavaScript Tricky Parts/Asynchronous.js
+++ JavaScript Tricky Parts/Asynchronous.js
@@ -12,36 +12,55 @@ function addItForMe(num1, num2, cb) {
   const result = num1 + num2;
   cb(result);
 }
+// another example
+function fetchData(cb) {
+  setTimeout(() => {
+    let data = { pCode: 1001, pName: "Green" };
+    cb(data);
+  }, 800);
+}
+console.log("start");
+fetchData(function (data) {
+  console.log(data.pName);
+  console.log("end");
+});
 
 // Promises - is a JS object
-const myPromise = new Promise((resolve, reject) => {
-  // Pending
+let promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("IT WORKS!");
-    //fulfilled:
+    resolve("Success");
 
     // reject("rejected");
     // //rejected
-  }, 2000);
+  }, 800);
 });
-
-// then executes in order
-myPromise.then((data) => data).then((data) => console.log(data));
+promise
+  .then((msg) => console.log(msg))
+  .catch((err) => console.log(err))
+  .finally(() => console.log("finishing process"));
+// Only those without error
+Promise.all([promise]).then(() => console.log("all promises resolved!"));
+// All will run one by one (even with error)
+Promise.allSettled([promise]).then(() => console.log("all promises resolved!"));
+// Fastest will return!
+Promise.race([promise]);
 
 // Example - Only works in browser
-// fetch("https://jsonplaceholder.typicode.com/todos/1", {})
-//   .then((res) => res.json())
-//   .then(
-//     (res) => {
-//       res.json();
-//     },
-//     (error) => {
-//       console.log(error);
-//     }
-//   ) // when failed
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error))
-//   .finally(() => console.log("finally"));
+/*
+fetch("https://jsonplaceholder.typicode.com/todos/1", {})
+  .then((res) => res.json())
+  .then(
+    (res) => {
+      res.json();
+    },
+    (error) => {
+      console.log(error);
+    }
+  ) // when failed
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("finally"));
+*/
 
 // Older APIs (like setTimeout) don’t use Promises but since you can create your own promises as well, you would be able to create a promise wrapper
 
@@ -57,7 +76,42 @@ setTimer(2000).then(() => console.log("setTimer completed!"));
 
 // Async Await - use promisees under the hood
 async function timer() {
+  // await -> wait till promise is resolved
   await setTimer(3000);
   console.log("async timer works");
 }
 timer();
+// another example
+(async function test() {
+  try {
+    const result = await setTimeout(() => {
+      console.log("resolved!");
+    }, 800);
+    return result;
+  } catch {
+    console.log("error!");
+  }
+})();
+
+// AJAX - XMLHTTPRequest (XHR) - only works in browser
+/*
+let request = new XMLHttpRequest();
+request.open("GET", "https://jsonplaceholder.typicode.com/comments");
+request.responseType = "json"; // "", "text"
+request.send();
+request.onload = function () {
+  if (request.status === 200 && request.readyState === 4) {
+    console.log(request.response);
+    console.log(request.getResponseHeader);
+  }
+};
+request.ontimeout = function () {
+  console.log("timed out...");
+};
+*/
+
+// Async Iterators & Generators
+/*
+for await (const iterator of object) {
+}
+*/
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JavaScript/TypeScript Advanced Concepts
+# JavaScript/TypeScript Notebook
 
 <h2 align="center">
   <img src="photo.png" width="500px" />
